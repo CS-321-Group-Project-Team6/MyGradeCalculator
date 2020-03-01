edc7de679a68343bcf64e55d0fe3c9fafdf6f744
CMertz <carrie.mertz@outlook.com>
2020-03-01 17:48:44 -0500

    This is my first commit - Carrie

--- app/src/main/java/com/example/mygradecalculator/MainActivity.java
+++ app/src/main/java/com/example/mygradecalculator/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
+    // this is what creates an instance of the app
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
