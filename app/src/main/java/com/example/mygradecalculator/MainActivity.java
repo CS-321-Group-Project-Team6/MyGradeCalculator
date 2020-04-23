fd726536c668073aa6abc35e176a75a950f619ba
CMertz <carrie.mertz@outlook.com>
2020-04-23 17:22:52 -0400

    Caluculator Fragment GUI Complete. Needs Backgound handlers and calculator functions to be mapped to textboxes and onClick for buttons. See .xml for control names.

--- app/src/main/java/com/example/mygradecalculator/MainActivity.java
+++ app/src/main/java/com/example/mygradecalculator/MainActivity.java
@@ -34,6 +34,8 @@ public class MainActivity extends AppCompatActivity{
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+
     }
 
     @Override
--- app/src/main/java/com/example/mygradecalculator/ui/calculator/CalculatorViewModel.java
+++ app/src/main/java/com/example/mygradecalculator/ui/calculator/CalculatorViewModel.java
@@ -1,5 +1,8 @@
 package com.example.mygradecalculator.ui.calculator;
 
+import android.view.View;
+import android.widget.Button;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -8,10 +11,12 @@ public class CalculatorViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
+
     public CalculatorViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is the calculator fragment");
+        mText.setValue("This is the calculator");
     }
+
 
     public LiveData<String> getText() {
         return mText;
