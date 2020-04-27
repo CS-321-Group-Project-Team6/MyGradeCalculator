fa6ea36a7e483f0dad6d741207e05d807449b469
YDessalene <ydessalene@gmail.com>
2020-04-27 01:45:15 -0400

    I Deleted the Home Fragment Class and all associated references to it due to it being unused. I reordered the navigation bar to show the class fragment first, and changed the icons to better reflect the options.

--- app/src/main/java/com/example/mygradecalculator/MainActivity.java
+++ app/src/main/java/com/example/mygradecalculator/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity{
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_calculator, R.id.nav_classes)
+                R.id.nav_classes,R.id.nav_calculator)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
