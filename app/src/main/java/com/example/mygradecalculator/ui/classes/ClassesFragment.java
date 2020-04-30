54f7a8a904d5927bbfc6f10b4dd84c365724e016
YDessalene <ydessalene@gmail.com>
2020-04-30 17:47:47 -0400

    Backend for the adding of classes in ClassesFragment.

--- app/src/main/java/com/example/mygradecalculator/ui/classes/ClassesFragment.java
+++ app/src/main/java/com/example/mygradecalculator/ui/classes/ClassesFragment.java
@@ -1,10 +1,13 @@
 package com.example.mygradecalculator.ui.classes;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ScrollView;
@@ -24,7 +27,7 @@ import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
-public class ClassesFragment extends Fragment implements View.OnClickListener{
+public class ClassesFragment extends Fragment implements View.OnClickListener, View.OnFocusChangeListener{
 
     private ClassesViewModel classesViewModel;
     private LayoutInflater classesInflater;
@@ -37,6 +40,35 @@ public class ClassesFragment extends Fragment implements View.OnClickListener{
     private TextView textView;
     private ViewGroup classesContainer;
     private View root;
+
+    private TextView gradeTxt1;
+    private EditText grade1;
+    private EditText weight1;
+    private ArrayList<Integer> grades1 = new ArrayList<Integer>();
+
+    private TextView gradeTxt2;
+    private EditText grade2;
+    private EditText weight2;
+    private ArrayList<Integer> grades2 = new ArrayList<Integer>();
+
+    private TextView gradeTxt3;
+    private EditText grade3;
+    private EditText weight3;
+    private ArrayList<Integer> grades3 = new ArrayList<Integer>();
+
+    private TextView gradeTxt4;
+    private EditText grade4;
+    private EditText weight4;
+    private ArrayList<Integer> grades4 = new ArrayList<Integer>();
+
+    private TextView gradeTxt5;
+    private EditText grade5;
+    private EditText weight5;
+    private ArrayList<Integer> grades5 = new ArrayList<Integer>();
+
+    private EditText className;
+    private int classGrade;
+
     private ScrollView parent;
     private Button addClass;
 
@@ -88,9 +120,7 @@ public class ClassesFragment extends Fragment implements View.OnClickListener{
                                    //since these are supposed to be ongoing academic classes
             classesInflater.inflate(R.layout.add_class_menu, (ViewGroup) root);
             classes_fab.setVisibility(FloatingActionButton.GONE);
-            addClass = root.findViewById(R.id.addClass);
-            addClass.setOnClickListener(this);
-            parent = root.findViewById(R.id.add_class_parent);
+            initAddClass();
             //classList.add(new ClassModel("", 0.0));
             //classesAdapter.notifyDataSetChanged(); //This function is necessary to call after any changes to the list
         }                                          //are made. Otherwise changes wont show up in the emulator view window
@@ -106,19 +136,75 @@ public class ClassesFragment extends Fragment implements View.OnClickListener{
         }
     }
 
-
-
     public MyClassAdapter.ViewHolder removeClassItem() {
         return null;
     }
 
     @Override
     public void onClick(View v) {
+        String text;
         switch(v.getId()) {
             case R.id.classes_fab:
                 createNewClassItem();
                 break;
+            case R.id.addClassAdd1:
+                if (grade1.getText().length() != 0) {
+                    text = gradeTxt1.getText() + " " + grade1.getText() + "%";
+                    gradeTxt1.setText(text);
+                    grades1.add(Integer.parseInt(String.valueOf(grade1.getText())));
+                }
+                break;
+            case R.id.addClassClear1:
+                gradeTxt1.setText("");
+                grades1.clear();
+                break;
+            case R.id.addClassAdd2:
+                if (grade2.getText().length() != 0) {
+                    text = gradeTxt2.getText() + " " + grade2.getText() + "%";
+                    gradeTxt2.setText(text);
+                    grades2.add(Integer.parseInt(String.valueOf(grade2.getText())));
+                }
+                break;
+            case R.id.addClassClear2:
+                gradeTxt2.setText("");
+                grades2.clear();
+                break;
+            case R.id.addClassAdd3:
+                if (grade3.getText().length() != 0) {
+                    text = gradeTxt3.getText() + " " + grade3.getText() + "%";
+                    gradeTxt3.setText(text);
+                    grades3.add(Integer.parseInt(String.valueOf(grade3.getText())));
+                }
+                break;
+            case R.id.addClassClear3:
+                gradeTxt3.setText("");
+                grades3.clear();
+                break;
+            case R.id.addClassAdd4:
+                if (grade4.getText().length() != 0) {
+                    text = gradeTxt4.getText() + " " + grade4.getText() + "%";
+                    gradeTxt4.setText(text);
+                    grades4.add(Integer.parseInt(String.valueOf(grade4.getText())));
+                }
+                break;
+            case R.id.addClassClear4:
+                gradeTxt4.setText("");
+                grades4.clear();
+                break;
+            case R.id.addClassAdd5:
+                if (grade5.getText().length() != 0) {
+                    text = gradeTxt5.getText() + " " + grade5.getText() + "%";
+                    gradeTxt5.setText(text);
+                    grades5.add(Integer.parseInt(String.valueOf(grade5.getText())));
+                }
+                break;
+            case R.id.addClassClear5:
+                gradeTxt5.setText("");
+                grades5.clear();
+                break;
+
             case R.id.addClass:
+                classGrade = calculate();
                 parent.setVisibility(ScrollView.GONE);
                 classesInflater.inflate(R.layout.fragment_classes, (ViewGroup) root);
                 classes_fab.setVisibility(FloatingActionButton.GONE);
@@ -126,5 +212,132 @@ public class ClassesFragment extends Fragment implements View.OnClickListener{
                 classesAdapter.notifyDataSetChanged();
                 break;
         }
+    }
+
+    public void initAddClass() {
+        className = root.findViewById(R.id.addClassName);
+        gradeTxt1 = root.findViewById(R.id.addClassGradesTxt1);
+        gradeTxt1.setMovementMethod(new ScrollingMovementMethod());
+        grade1 = root.findViewById(R.id.addClassGrade1);
+        grade1.setOnFocusChangeListener((View.OnFocusChangeListener) this);
+        weight1 = root.findViewById(R.id.addClassWeight1);
+        weight1.setOnFocusChangeListener((View.OnFocusChangeListener) this);
+        Button addGrade1 = root.findViewById(R.id.addClassAdd1);
+        addGrade1.setOnClickListener(this);
+        Button clearGrade1 = root.findViewById(R.id.addClassClear1);
+        clearGrade1.setOnClickListener(this);
+
+        gradeTxt2 = root.findViewById(R.id.addClassGradesTxt2);
+        gradeTxt2.setMovementMethod(new ScrollingMovementMethod());
+        grade2 = root.findViewById(R.id.addClassGrade2);
+        grade2.setOnFocusChangeListener((View.OnFocusChangeListener) this);
+        weight2 = root.findViewById(R.id.addClassWeight2);
+        weight2.setOnFocusChangeListener((View.OnFocusChangeListener) this);
+        Button addGrade2 = root.findViewById(R.id.addClassAdd2);
+        addGrade2.setOnClickListener(this);
+        Button clearGrade2 = root.findViewById(R.id.addClassClear2);
+        clearGrade2.setOnClickListener(this);
+
+        gradeTxt3 = root.findViewById(R.id.addClassGradesTxt3);
+        gradeTxt3.setMovementMethod(new ScrollingMovementMethod());
+        grade3 = root.findViewById(R.id.addClassGrade3);
+        grade3.setOnFocusChangeListener((View.OnFocusChangeListener) this);
+        weight3 = root.findViewById(R.id.addClassWeight3);
+        weight3.setOnFocusChangeListener((View.OnFocusChangeListener) this);
+        Button addGrade3 = root.findViewById(R.id.addClassAdd3);
+        addGrade3.setOnClickListener(this);
+        Button clearGrade3 = root.findViewById(R.id.addClassClear3);
+        clearGrade3.setOnClickListener(this);
+
+        gradeTxt4 = root.findViewById(R.id.addClassGradesTxt4);
+        gradeTxt4.setMovementMethod(new ScrollingMovementMethod());
+        grade4 = root.findViewById(R.id.addClassGrade4);
+        grade4.setOnFocusChangeListener((View.OnFocusChangeListener) this);
+        weight4 = root.findViewById(R.id.addClassWeight4);
+        weight4.setOnFocusChangeListener((View.OnFocusChangeListener) this);
+        Button addGrade4 = root.findViewById(R.id.addClassAdd4);
+        addGrade4.setOnClickListener(this);
+        Button clearGrade4 = root.findViewById(R.id.addClassClear4);
+        clearGrade4.setOnClickListener(this);
+
+        gradeTxt5 = root.findViewById(R.id.addClassGradesTxt5);
+        gradeTxt5.setMovementMethod(new ScrollingMovementMethod());
+        grade5 = root.findViewById(R.id.addClassGrade5);
+        grade5.setOnFocusChangeListener((View.OnFocusChangeListener) this);
+        weight5 = root.findViewById(R.id.addClassWeight5);
+        weight5.setOnFocusChangeListener((View.OnFocusChangeListener) this);
+        Button addGrade5 = root.findViewById(R.id.addClassAdd5);
+        addGrade5.setOnClickListener(this);
+        Button clearGrade5 = root.findViewById(R.id.addClassClear5);
+        clearGrade5.setOnClickListener(this);
+
+        addClass = root.findViewById(R.id.addClass);
+        addClass.setOnClickListener(this);
+        parent = root.findViewById(R.id.add_class_parent);
+    }
+
+    private int calculate() {
+        /*
+        values:
+        grades1 holds values for weight1, and so on.
+        grades1-5 are ArrayLists
+         */
+        int grade = 0;
+        double average = 0;
+
+        if (weight1.getText().length() != 0) {
+            for (int n : grades1)
+                average += n;
+            average /= grades1.size();
+            average /= 100;
+            grade += (Integer.parseInt(String.valueOf(weight1.getText())) * average);
+            average = 0;
+        }
+        if (weight2.getText().length() != 0) {
+            for (int n : grades2)
+                average += n;
+            average /= grades2.size();
+            average /= 100;
+            grade += (Integer.parseInt(String.valueOf(weight2.getText())) * average);
+            average = 0;
+        }
+        if (weight3.getText().length() != 0) {
+            for (int n : grades3)
+                average += n;
+            average /= grades3.size();
+            average /= 100;
+            grade += (Integer.parseInt(String.valueOf(weight3.getText())) * average);
+            average = 0;
+        }
+        if (weight4.getText().length() != 0) {
+            for (int n : grades4)
+                average += n;
+            average /= grades4.size();
+            average /= 100;
+            grade += (Integer.parseInt(String.valueOf(weight4.getText())) * average);
+            average = 0;
+        }
+        if (weight5.getText().length() != 0) {
+            for (int n : grades5)
+                average += n;
+            average /= grades5.size();
+            average /= 100;
+            grade += (Integer.parseInt(String.valueOf(weight5.getText())) * average);
+        }
+
+        return grade;
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        if (!hasFocus)
+            hideKeyboard();
+    }
+
+    private void hideKeyboard() {
+        View v = getView();
+        InputMethodManager inputMethodManager = (InputMethodManager) getActivity()
+                .getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
     }
 }
